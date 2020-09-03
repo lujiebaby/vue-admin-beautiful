fe1623e52797ff6a7c653065f57cf5125eb6a31f
chuzhixin <1204505056@qq.com>
2020-09-03 23:02:53 +0800

    deploy

---
+++ static/js/chunk-2d224581.802eab17.js
@@ -0,0 +1,6 @@
+/*!
+ *  build: vue-admin-beautiful 
+ *  copyright: chuzhixin 1204505056@qq.com 
+ *  time: 2020-9-3 23:02:17
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d224581"],{e077:function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"goodsDetail-container"})},c=[],o={name:"GoodsDetail",data:function(){return{}},created:function(){},methods:{}},s=o,i=e("9ca4"),u=Object(i["a"])(s,a,c,!1,null,null,null);t["default"]=u.exports}}]);
